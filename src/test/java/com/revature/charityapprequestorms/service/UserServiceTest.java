50e298d13629e18a0bb6f772de6e9d972c91b119
Keyneloui <keyne.loui@gmail.com>
2019-11-08 15:40:18 +0530

    Deleted Controller Test and added UserServiceTest

--- src/test/java/com/revature/charityapprequestorms/service/UserServiceTest.java
+++ src/test/java/com/revature/charityapprequestorms/service/UserServiceTest.java
@@ -6,14 +6,48 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.revature.charityapprequestorms.dto.UserDTO;
+
+
 @SpringBootTest
 class UserServiceTest {
 	@Autowired
 	UserService userService;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void validTest() {
+		
+		Boolean userDto=null;
+		int userId=1;
+		userDto=userService.validateId(userId);
+		assertNotNull(userDto);
+		
+	}
+	@Test
+	void inValidTest() {
+		
+		Boolean userDto=null;
+		int userId=12;
+		userDto=userService.validateId(userId);
+		assertFalse(userDto);
+		
+	}
+	@Test
+	void validIdTest()
+	{
+		UserDTO userDto=null;
+		int userId=1;
+		userDto=userService.getUser(userId);
+		assertNotNull(userDto);
+	}
+	@Test
+	void inValidIdTest()
+	{
+		UserDTO userDto=null;
+		int id=12;
+		
+		userDto=userService.getUser(id);
+		assertNull(userDto.getId());
 	}
 
 }
